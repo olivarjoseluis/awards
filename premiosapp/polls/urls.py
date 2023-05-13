b031e6d2503a08a31adc1332427a5e81b3705776
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-05-12 23:18:11 -0500

    Views for polls

--- premiosapp/polls/urls.py
+++ premiosapp/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="index"),
+    path("<int:question_id>/results/", views.result, name="index"),
+    path("<int:question_id>/votes/", views.votes, name="index")
 ]
--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Home")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta # {question_id}")
+
+
+def result(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados # {question_id}")
+
+
+
+def votes(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta # {question_id}")
