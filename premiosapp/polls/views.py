783c52ca0ac7c40eebb365f11a5a1a922805ad28
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-05-15 21:46:03 -0500

    Question internal template

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+
 
 def index(request):
     lates_question_list = Question.objects.all()
     return render(request, "polls/index.html", {"lates_question_list":lates_question_list})
 
 def detail(request, question_id):
-    return 
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def result(request, question_id):
     return HttpResponse(f"Estas viendo los resultados # {question_id}")
