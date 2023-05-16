459b1aedde308ef9eb4e179fea92fe38bd349a71
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-05-15 20:44:21 -0500

    Home template and Question variable

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,19 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
-
+from .models import Question
 
 def index(request):
-    return HttpResponse("Home")
-
+    lates_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {"lates_question_list":lates_question_list})
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta # {question_id}")
-
+    return 
 
 def result(request, question_id):
     return HttpResponse(f"Estas viendo los resultados # {question_id}")
-
-
 
 def votes(request, question_id):
     return HttpResponse(f"Estas votando a la pregunta # {question_id}")
