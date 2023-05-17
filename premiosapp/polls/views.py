0fdf2daa4ab492a997691da2ab2a1ac00e6302eb
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-05-16 22:57:35 -0500

    View and vote

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Question,Choice
 
 
 def index(request):
@@ -12,7 +14,16 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 def result(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados # {question_id}")
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/results.html",{"question":question})
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta # {question_id}")
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html", {"question":question,"error_message":"Respuesta no elegida"})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
